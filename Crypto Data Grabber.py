5bedb1299531bc51632e80db170dbfac7bf06770
Ahsan <ahsan.nab.n6@gmail.com>
2021-01-24 23:29:49 +0530

    Only one column build

--- Crypto Data Grabber.py
+++ Crypto Data Grabber.py
@@ -4,9 +4,9 @@ import requests
 r=requests.get('https://api.wazirx.com/api/v2/tickers')
 data=r.json()
 df=pd.DataFrame(data)
-df2=pd.read_excel("Crypto.xlsx")
+df2=pd.read_excel("Crypto.xlsx",index_col='Ticker')
 tickers= list(df2.index)
 
 for i in tickers:
     df2['Last'][i]=df[i]['last']
-    df2['Profit/Loss'][i]=(df2['Last'][i]-df2['Average'][i])*df2['Holdings'][i]
+df2.to_excel("Crypto.xlsx")
