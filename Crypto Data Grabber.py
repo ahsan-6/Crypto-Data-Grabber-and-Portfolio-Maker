ea4183e2bd1749cef97c77aba0bafacd3afd8229
Ahsan <ahsan.nab.n6@gmail.com>
2021-01-25 16:11:45 +0530

    Optimised code

--- Crypto Data Grabber.py
+++ Crypto Data Grabber.py
@@ -2,11 +2,9 @@ import pandas as pd
 import requests
 
 r=requests.get('https://api.wazirx.com/api/v2/tickers')
-data=r.json()
-df=pd.DataFrame(data)
 df2=pd.read_excel("Crypto.xlsx",index_col='Ticker')
 tickers= list(df2.index)
 
 for i in tickers:
-    df2['Last'][i]=df[i]['last']
+    df2['Last'][i]=r.json()[i]['last']
 df2.to_excel("Crypto.xlsx")
