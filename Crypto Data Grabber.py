98e0ce505af088b26a3873a8bede640f4a0eacb2
Ahsan <ahsan.nab.n6@gmail.com>
2021-01-27 22:40:18 +0530

    added support for newer version of xlrd

--- Crypto Data Grabber.py
+++ Crypto Data Grabber.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 
 r=requests.get('https://api.wazirx.com/api/v2/tickers')
-df2=pd.read_excel("Crypto.xlsx",index_col='Ticker')
+df2=pd.read_excel("Crypto.xlsx",engine="openpyxl",index_col='Ticker')
 tickers= list(df2.index)
 
 for i in tickers:
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CryptoDataGrabber
 This program grabs the last price of cryptocurrencies from WazirX exchange database using the WazirX API and puts it in an excel file.
+
+Python libraries required - pandas,xlrd,requests
