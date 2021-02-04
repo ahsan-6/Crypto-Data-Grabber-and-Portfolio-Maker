5545e2745ca0036a7c0ec8b5910ccceb45987f29
Ahsan <ahsan.nab.n6@gmail.com>
2021-02-04 23:11:42 +0530

    fixed null index error

--- Crypto Data Grabber.py
+++ Crypto Data Grabber.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import requests
+import math
 
 r=requests.get('https://api.wazirx.com/api/v2/tickers')
-df2=pd.read_excel("Crypto.xlsx",engine="openpyxl",index_col='Ticker')
+df2=pd.read_excel("Crypto2.xlsx",engine="openpyxl",index_col='Ticker')
 tickers= list(df2.index)
 
 for i in tickers:
-    df2['Last'][i]=r.json()[i]['last']
-df2.to_excel("Crypto.xlsx")
+    if i in r.json():
+            df2['Last'][i]=r.json()[i]['last']
+    else: continue
+df2.to_excel("Crypto2.xlsx")
